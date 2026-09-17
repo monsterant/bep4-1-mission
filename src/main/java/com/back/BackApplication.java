1f436642469dfea0ef7b08a5860acc553e23ce79
monsterant <pjb19907@gmail.com>
2026-09-17 12:28:08 +0900

    0001

--- src/main/java/com/back/BackApplication.java
+++ src/main/java/com/back/BackApplication.java
@@ -1,9 +1,10 @@
 package com.back;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackApplication {
 
     public static void main(String[] args) {
