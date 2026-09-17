906a3a0174ba3c1c7c8de02f35939032ca8fc08f
monsterant <pjb19907@gmail.com>
2026-09-17 13:30:56 +0900

    0002

--- src/main/java/com/back/initData/DataInit.java
+++ src/main/java/com/back/initData/DataInit.java
@@ -8,28 +8,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.transaction.annotation.Transactional;
+import com.back.entity.Post;
+import com.back.service.PostService;
 
 @Configuration
 @Slf4j
 public class DataInit {
     private final DataInit self;
     private final MemberService memberService;
+    private final PostService postService;
 
-    public DataInit(@Lazy DataInit self, MemberService memberService) {
+    public DataInit(
+            @Lazy DataInit self,
+            MemberService memberService,
+            PostService postService
+    ) {
         this.self = self;
         this.memberService = memberService;
+        this.postService = postService;
     }
 
     @Bean
     public ApplicationRunner baseInitDataRunner() {
         return args -> {
             self.makeBaseMembers();
+            self.makeBasePosts();
         };
     }
 
     @Transactional
     public void makeBaseMembers() {
-        if (memberService.count() > 0) return;
+        if (memberService.count() > 0){
+            return;
+        }
 
         Member systemMember = memberService.join("system", "1234", "시스템");
         Member holdingMember = memberService.join("holding", "1234", "홀딩");
@@ -38,4 +49,23 @@ public class DataInit {
         Member user2Member = memberService.join("user2", "1234", "유저2");
         Member user3Member = memberService.join("user3", "1234", "유저3");
     }
+
+    @Transactional
+    public void makeBasePosts() {
+        if (postService.count() > 0){
+            return;
+        }
+
+        Member user1Member = memberService.findByUsername("user1").orElseThrow(() -> new RuntimeException("회원 없음"));
+        Member user2Member = memberService.findByUsername("user2").orElseThrow(() -> new RuntimeException("회원 없음"));
+        Member user3Member = memberService.findByUsername("user3").orElseThrow(() -> new RuntimeException("회원 없음"));
+
+        Post post1 = postService.write(user1Member, "제목1", "내용1");
+        Post post2 = postService.write(user1Member, "제목2", "내용2");
+        Post post3 = postService.write(user1Member, "제목3", "내용3");
+        Post post4 = postService.write(user2Member, "제목4", "내용4");
+        Post post5 = postService.write(user2Member, "제목5", "내용5");
+        Post post6 = postService.write(user3Member, "제목6", "내용6");
+    }
+
 }
