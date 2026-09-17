6902c690df61bf60e323c16a9c3649d71c3038de
monsterant <pjb19907@gmail.com>
2026-09-17 19:02:42 +0900

    0009

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -1,23 +1,19 @@
-
 package com.back.boundedContext.post.app;
-import com.back.global.eventPublisher.EventPublisher;
-import com.back.shared.post.dto.PostDto;
-import com.back.shared.post.event.PostCreatedEvent;
+
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
+import com.back.global.eventPublisher.EventPublisher;
+import com.back.shared.post.dto.PostDto;
+import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class PostService {
+public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
-
-    public long count() {
-        return postRepository.count();
-    }
 
     public Post write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
@@ -29,9 +25,5 @@ public class PostService {
         );
 
         return post;
-    }
-
-    public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
     }
 }
--- src/main/java/com/back/global/initData/DataInit.java
+++ src/main/java/com/back/global/initData/DataInit.java
@@ -2,31 +2,30 @@ package com.back.global.initData;
 
 import com.back.boundedContext.member.app.MemberFacade;
 import com.back.boundedContext.member.domain.Member;
-
+import com.back.boundedContext.post.app.PostFacade;
+import com.back.boundedContext.post.domain.Post;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.transaction.annotation.Transactional;
-import com.back.boundedContext.post.domain.Post;
-import com.back.boundedContext.post.app.PostService;
 
 @Configuration
 @Slf4j
 public class DataInit {
     private final DataInit self;
     private final MemberFacade memberFacade;
-    private final PostService postService;
+    private final PostFacade postFacade;
 
     public DataInit(
             @Lazy DataInit self,
             MemberFacade memberFacade,
-            PostService postService
+            PostFacade postFacade
     ) {
         this.self = self;
         this.memberFacade = memberFacade;
-        this.postService = postService;
+        this.postFacade = postFacade;
     }
 
     @Bean
@@ -54,7 +53,7 @@ public class DataInit {
 
     @Transactional
     public void makeBasePosts() {
-        if (postService.count() > 0){
+        if (postFacade.count() > 0){
             return;
         }
 
@@ -62,12 +61,12 @@ public class DataInit {
         Member user2Member = memberFacade.findByUsername("user2").orElseThrow(() -> new RuntimeException("회원 없음"));
         Member user3Member = memberFacade.findByUsername("user3").orElseThrow(() -> new RuntimeException("회원 없음"));
 
-        Post post1 = postService.write(user1Member, "제목1", "내용1");
-        Post post2 = postService.write(user1Member, "제목2", "내용2");
-        Post post3 = postService.write(user1Member, "제목3", "내용3");
-        Post post4 = postService.write(user2Member, "제목4", "내용4");
-        Post post5 = postService.write(user2Member, "제목5", "내용5");
-        Post post6 = postService.write(user3Member, "제목6", "내용6");
+        Post post1 = postFacade.write(user1Member, "제목1", "내용1");
+        Post post2 = postFacade.write(user1Member, "제목2", "내용2");
+        Post post3 = postFacade.write(user1Member, "제목3", "내용3");
+        Post post4 = postFacade.write(user2Member, "제목4", "내용4");
+        Post post5 = postFacade.write(user2Member, "제목5", "내용5");
+        Post post6 = postFacade.write(user3Member, "제목6", "내용6");
     }
 
     @Transactional
@@ -77,10 +76,10 @@ public class DataInit {
         Member user1Member = memberFacade.findByUsername("user1").orElseThrow(() -> new RuntimeException("회원 없음"));
         Member user2Member = memberFacade.findByUsername("user2").orElseThrow(() -> new RuntimeException("회원 없음"));
         Member user3Member = memberFacade.findByUsername("user3").orElseThrow(() -> new RuntimeException("회원 없음"));
-        Post post1 = postService.findById(1).orElseThrow();
-        Post post2 = postService.findById(2).orElseThrow();
-        Post post3 = postService.findById(3).orElseThrow();
-        Post post4 = postService.findById(4).orElseThrow();
+        Post post1 = postFacade.findById(1).orElseThrow();
+        Post post2 = postFacade.findById(2).orElseThrow();
+        Post post3 = postFacade.findById(3).orElseThrow();
+        Post post4 = postFacade.findById(4).orElseThrow();
         post1.addComment(user1Member,"댓글1");
         post1.addComment(user2Member,"댓글2");
         post1.addComment(user3Member,"댓글3");
