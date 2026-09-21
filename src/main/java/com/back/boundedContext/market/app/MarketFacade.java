47526e36f417e71e6f836e83e30567f040f740fe
monsterant <pjb19907@gmail.com>
2026-09-21 11:42:13 +0900

    030

---
+++ src/main/java/com/back/boundedContext/market/app/MarketFacade.java
@@ -0,0 +1,17 @@
+package com.back.boundedContext.market.app;
+import org.springframework.transaction.annotation.Transactional;
+import com.back.boundedContext.market.domain.MarketMember;
+import com.back.shared.member.dto.MemberDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MarketFacade {
+    private final MarketSyncMemberUseCase marketSyncMemberUseCase;
+
+    @Transactional
+    public MarketMember syncMember(MemberDto member) {
+        return marketSyncMemberUseCase.syncMember(member);
+    }
+}
