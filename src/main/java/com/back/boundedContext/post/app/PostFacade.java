7bd55360e2fde10448018c205fae0e02b683ebe0
monsterant <pjb19907@gmail.com>
2026-09-21 10:57:19 +0900

    028

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -2,8 +2,6 @@ package com.back.boundedContext.post.app;
 
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.domain.PostMember;
-import com.back.boundedContext.post.out.PostMemberRepository;
-import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.rsData.RsData;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
@@ -15,14 +13,16 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class PostFacade {
-    private final PostRepository postRepository;
     private final PostWriteUseCase postWriteUseCase;
-    private final PostMemberRepository postMemberRepository;
+    private final PostSupport postSupport;
+    private final PostSyncMemberUseCase postSyncMemberUseCase;
+
 
     @Transactional(readOnly = true)
     public long count() {
-        return postRepository.count();
+        return postSupport.count();
     }
+
 
     @Transactional
     public RsData<Post> write(PostMember author, String title, String content) {
@@ -31,26 +31,19 @@ public class PostFacade {
 
     @Transactional(readOnly = true)
     public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
+        return postSupport.findById(id);
     }
-    @Transactional
-    public PostMember syncMember(MemberDto member) {
-        PostMember _member = new PostMember(
-                member.getId(),
-                member.getCreateDate(),
-                member.getModifyDate(),
-                member.getUsername(),
-                "",
-                member.getNickname(),
-                member.getActivityScore()
-        );
 
-        return postMemberRepository.save(_member);
-    }
+
 
     @Transactional(readOnly = true)
     public Optional<PostMember> findMemberByUsername(String username) {
-        return postMemberRepository.findByUsername(username);
+        return postSupport.findMemberByUsername(username);
+    }
+
+    @Transactional
+    public PostMember syncMember(MemberDto member) {
+        return postSyncMemberUseCase.syncMember(member);
     }
 
 }
