7e857542c1d32a8e29763b2357cdbf034ae6a719
monsterant <pjb19907@gmail.com>
2026-09-22 11:56:26 +0900

    036

--- src/main/java/com/back/boundedContext/market/app/MarketFacade.java
+++ src/main/java/com/back/boundedContext/market/app/MarketFacade.java
@@ -95,6 +95,7 @@ public class MarketFacade {
 
     @Transactional
     public void requestPayment(Order order, long pgPaymentAmount) {
+        order = marketSupport.findOrderById(order.getId()).orElseThrow();
         order.requestPayment(pgPaymentAmount);
     }
 
